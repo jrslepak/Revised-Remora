9732615921e4848d4293c19acdba6de746116cbb
Justin Slepak <jrslepak@ccs.neu.edu>
2017-11-13 12:09:36 -0500

    Define type erasure -- target language and erasure algorithm (still need automated tests)

---
+++ erased-lang.rkt
@@ -0,0 +1,67 @@
+#lang racket
+
+(require redex
+         ;"language.rkt"
+         "typing-rules.rkt")
+
+(define-extended-language Remora-erased Remora-annotated
+  (atom:e base-val
+          op
+          (λ ((x type:e) ...) expr:e)
+          (Iλ ((x sort) ...) val:e)
+          (box idx ... expr:e type:e))
+  (atval:e base-val
+           op
+           (λ ((x type:e) ...) expr:e)
+           (Iλ ((x sort) ...) val:e)
+           (box idx ... val:e type:e))
+  (expr:e var
+          (array {natural ...} [atom:e ...])
+          (array {natural ...} [expr:e ...])
+          (expr:e expr:e ... type:e)
+          (i-app expr:e idx ... type:e)
+          (unbox (var ... var expr:e) expr:e))
+  (val:e var
+         (array {natural ...} atval:e ...))
+  (type:e flat
+          (Σ [(var sort) ...] type:e)
+          (Array type:e idx)))
+
+(define-metafunction Remora-erased
+  erase-type : type -> type:e
+  [(erase-type (Array type idx)) (Array (erase-type type) idx)]
+  [(erase-type (Σ [(var sort) ...] type:e)) (Σ [(var sort) ...] (erase-type type))]
+  [(erase-type (Π _ type)) (erase-type type)]
+  [(erase-type (∀ _ type)) (erase-type type)]
+  [(erase-type var) flat]
+  [(erase-type (-> _ _)) flat]
+  [(erase-type base-type) flat])
+
+(define-metafunction Remora-erased
+  erase-atom : atom:t -> atom:e
+  [(erase-atom base-val) base-val]
+  [(erase-atom op) op]
+  [(erase-atom (λ [var ...] expr:t : (-> [type_in ...] type_out)))
+   (λ [(var (erase-type type_in)) ...] (erase-expr expr:t))]
+  [(erase-atom (tλ [var ...] val:t : type))
+   (erase-atom val:t)]
+  [(erase-atom (iλ [var ...] val:t : type))
+   (iλ [var ...] (erase-atom val:t))]
+  [(erase-atom (box idx ... expr:t : type))
+   (box idx ... (erase-expr expr:t) (erase-type type))])
+
+(define-metafunction Remora-erased
+  erase-expr : expr:t -> expr:e
+  [(erase-expr (var : type)) var]
+  [(erase-expr (array {natural ...} [atom:t ...] : type))
+   (array {natural ...} [(erase-atom atom:t) ...])]
+  [(erase-expr (frame {natural ...} [expr:t ...] : type))
+   (frame {natural ...} [(erase-expr expr:t) ...])]
+  [(erase-expr (expr:t_f expr:t_a ... : type))
+   ((erase-expr expr:t_f) (erase-expr expr:t_a) ... : (erase-type type))]
+  [(erase-expr (t-app expr:t_f type_a ... : type))
+   (erase-expr expr:t_f)]
+  [(erase-expr (i-app expr:t_f idx_a ... : type))
+   (i-app (erase-expr expr:t_f) idx_a ... : (erase-type type))]
+  [(erase-expr (unbox (var_i ... var_e expr:t_s) expr:t_b : type))
+   (unbox (var_i ... var_e (erase-expr expr:t_s)) (erase-expr expr:t_b))])
